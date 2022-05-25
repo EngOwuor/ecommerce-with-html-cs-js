397806b6493083cb4bb7df0f8bf568c9b055fefe
robert owuor <owuorrobert76@gmail.com>
2022-05-25 14:27:32 +0300

    initialized project

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,34 @@
+
+var express = require('express');
+var path = require('path');
+var open = require('open');
+
+var port = 3000;
+var app = express();
+var router = express.Router();
+
+router.get('/',function(req,res){
+    res.sendFile(path.join(__dirname,'../src/index.html'));
+
+});
+router.get('/recipes.html',function(req,req){
+    req.sendFile(path.join(__dirname,"../src/recipes.html"));
+});
+
+router.get('/nutrition.html',function(req,res){
+    res.sendFile(path.join(__dirname,'../src/nutrition.html'));
+});
+router.get('/',function(req,res){
+    res.sendFile(path.join(__dirname,'../src/img/logo.png'));
+});
+app.use(express.static('src'));
+
+app.use("/",router);
+
+app.listen(port,function(err){
+    if (err){
+        console.log(err)
+    }else{
+        open('http://localhost:'+ port); 
+    }
+});
