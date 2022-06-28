70e1200b3ebc0fcfa680704371f2ce5365b075a4
robert owuor <owuorrobert76@gmail.com>
2022-06-28 17:17:52 +0300

    made page responsive

---
+++ src/script.js
@@ -0,0 +1,17 @@
+
+const navbar = document.getElementById("navbar");
+const bar = document.getElementById("bar");
+const close = document.getElementById('close');
+
+
+if(bar){
+    bar.addEventListener('click',()=>{
+        navbar.classList.add('active');
+    })
+};
+
+if(close){
+    close.addEventListener('click',()=>{
+        navbar.classList.remove('active');
+    })
+};
